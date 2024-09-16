beb0f2e6afa210e3c6bfcc2487384db0c955cf92
DESKTOP-LQT54H0\tegass <tegass@qq.com>
2024-09-16 13:13:34 +0800

    更改了链表函数

--- c_fuction/链表/liked_list.c
+++ c_fuction/链表/liked_list.c
@@ -1,39 +1,106 @@
 #include "liked_list.h"
 
+/**
+ * @brief  对链表进行向下遍历
+ * @param  struc_p    开始遍历点的链表地址
+ * @retval None
+ */
+void  linklist_traverse_down(linklist* struc_p)
+{
+   do{
+       /*user struct code start*/
+
+    /*user struct code end*/
+        struc_p = struc_p->next;
+    } while (struc_p!= NULL);
+    
+}
+
+/**
+ * @brief  对链表进行向上遍历
+ * @param  struc_p    开始遍历点的链表地址
+ * @retval None
+ */
+void  linklist_traverse_up(linklist* struc_p)
+{
+    do {
+        /*user struct code start*/
+
+        /*user struct code end*/
+        struc_p = struc_p->last;
+    } while (struc_p != NULL);
+
+}
+
+/**
+ * @brief  对新创建的链表进行初始化
+ * @param  struc_p    要初始化的链表地址
+ * @retval None
+ */
 void  linklist_init(linklist* struc_p)
 {
 
 }
-unsigned char linklist_new(linklist** struc_p)
+
+/**
+ * @brief  在末尾插入一个链表
+ * @param  insert_list  要插入的链表地址
+ * @retval None
+ */
+void linklist_insert_end(linklist* insert_list)
+{
+    if (tail == NULL)
+    {
+        tail = insert_list;
+        head = insert_list;
+    }
+    else
+    {
+        tail->next = insert_list;
+        insert_list->last = tail;
+        tail = insert_list;
+    }
+
+}
+
+/**
+ * @brief  新创建一个链表，并插入链表末尾
+ * @param  struc_p    要初始化的链表地址
+ * @retval 1-成功，0-失败
+ */
+unsigned char linklist_new(void)
 {
    linklist* temp;
-
+   if(linkpoolhead==NULL)
+   {
     temp=(linklist*)malloc(sizeof(linklist));
     if(temp==NULL)
         return 0;
     
     temp->next=NULL;
     temp->last=NULL;
-    *struc_p=temp;
-    
+    linklist_insert_end(temp);
+
     return 1;
+   }
+   else 
+   {
+        temp=linkpoolhead;
+        linkpoolhead=linkpoolhead->next;
+        temp->next=NULL;
+        linklist_insert_end(temp);
+        return 1;
+
+   }
 }
 
-void linklist_insert_end(linklist* insert_list)
-{
-    if(tail==NULL)
-    {
-        tail=insert_list;
-        head=insert_list;
-    }
-    else 
-    {
-        tail->next=insert_list;
-        insert_list->last=tail;
-        tail=insert_list;
-    }
 
-}
+
+/**
+ * @brief  将一个链表删除到链表池
+ * @param  list  要删除的链表地址
+ * @retval  1-成功，0-失败
+ */
 unsigned char linklist_deltopool(linklist* list)
 {
   linklist* temp=head;
@@ -67,21 +134,19 @@ unsigned char linklist_deltopool(linklist* list)
     }
     return 1;
 }
-void linklist_del(linklist* list)
+/**
+ * @brief  释放链表池内存
+ * @param  num  要释放的链表数量
+ * @retval None
+ */
+void linkpoolhead_free(int num)
 {
     linklist* temp;
-    if(head->next==NULL)
+    while (num--)
     {
-     head=NULL;
-     tail=NULL;
+        if (linkpoolhead == NULL)break;
+        temp = linkpoolhead;
+        linkpoolhead = linkpoolhead->next;
+        free(temp);
     }
-    else 
-    {
-        temp=list->last;
-        temp->next=list->next;
-    
-        temp=list->next;
-        temp->last=list->last;
-   }
-    free(list);
 }
--- c_fuction/链表/liked_list.h
+++ c_fuction/链表/liked_list.h
@@ -4,10 +4,12 @@
 #include "stdio.h"
 #include <stdlib.h>
 
+/*链表名称定义区*/
 #define linklist list_name
 #define head list_head_name
 #define tail list_tail_name
- 
+
+/*链表定义区*/
 typedef struct link_list
 {
 /*user struct code start*/
@@ -26,11 +28,13 @@ linklist *head,*tail;
 
 
 /*双向链表函数*/
-void  linklist_init(linklist* struc_p);/*需要用户自定义*/
-void linklist_del(linklist* list);
+void  linklist_traverse_down(linklist* struc_p);
+void  linklist_traverse_up(linklist* struc_p);
+void  linklist_init(linklist* struc_p);
+/*上述函数需要用户添加代码*/
+void linklist_insert_end(linklist* insert_list);
+unsigned char linklist_new(void);
 unsigned char linklist_deltopool(linklist* list);
-void likelink_insert_end(linklist* insert_list);
-unsigned char linklist_new(linklist** struc_p);
-
+void linkpoolhead_free(int num);
 
 #endif
