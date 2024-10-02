67fe64a774001ad8bf8566f4523964e0b42b059c
DESKTOP-LQT54H0\tegass <tegass@qq.com>
2024-10-02 22:30:46 +0800

    遍历实现创建和遍历

--- c_fuction/二叉树/binary_tree.c
+++ c_fuction/二叉树/binary_tree.c
@@ -1,43 +1,96 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "binary_tree.h"
 
+
+/*
+*@name:binary_tree_data_input
+*@function:二叉树数据输入
+*@return:数据 
+*/
+binary_tree_data_t* binary_tree_data_input(void)
+{
+    int temp = 0;
+    binary_tree_data_t* data = (binary_tree_data_t*)malloc(sizeof(binary_tree_data_t));
+    scanf("%d", &temp);
+    if (temp == -1)
+    {
+        free(data);
+        return NULL;
+    }
+    else
+    {
+        data->data = temp;
+        return data;
+    }
+        
+}
+
+
+/*
+*@name:binary_tree_create
+*@function:二叉树创建
+*@return:二叉树指针
+*/
 binary_tree* binary_tree_create(void)
 {
-    binary_tree* new_tree = (binary_tree*)malloc(sizeof(binary_tree));
-    if(new_tree == NULL)
+    binary_tree_data_t* data = NULL;;
+    data = binary_tree_data_input();
+    if(data == NULL)
     {
         return NULL;
     }
 
-    new_tree->size = 0;
-    new_tree->root = NULL;
+    binary_tree* new_tree = (binary_tree*)malloc(sizeof(binary_tree));
+    if(new_tree == NULL)
+    {
+        free(data);
+        return NULL;
+    }
+
+    new_tree->data = data;
+    new_tree->left=binary_tree_create();
+    new_tree->right=binary_tree_create();
 
     return new_tree;
+
 }
 
-int binary_tree_insert (binary_tree* tree, binary_tree_data_t* data)
+/*
+*@name:binary_tree_visit
+*@function:二叉树节点访问
+*@return:无
+*@param:tree 二叉树指针 level 层级
+*/
+void binary_tree_visit(binary_tree* tree,int level)
 {
-    if(tree == NULL||data==NULL)
+    if(tree == NULL)
+    {
+        return;
+    }
+
+    printf("第%d层 数据为%d\n", level, tree->data->data);
+}
+
+
+/*
+*@name:binary_tree_traverse
+*@function:二叉树遍历
+*@return:无
+*@param:tree 二叉树指针 level 当前层级
+*/
+int  binary_tree_traverse(binary_tree* tree,int level)
+{
+    if(tree == NULL)
     {
         return ERROR;
     }
 
-    if(tree->root == NULL)
-    {
-        tree->root = (binary_tree_node*)malloc(sizeof(binary_tree_node));
-        if(tree->root == NULL)
-        {
-            return ERROR;
-        }
+    binary_tree_visit(tree,level);
+    binary_tree_traverse(tree->left, level+1);
+    binary_tree_traverse(tree->right, level + 1);
 
-        tree->root->data = data;
-        tree->root->left = NULL;
-        tree->root->right = NULL;
 
-        return OK;
-    }
-    else 
-    {
-        return OK;
-    }
+    return OK;
 
 }
--- c_fuction/二叉树/binary_tree.h
+++ c_fuction/二叉树/binary_tree.h
@@ -4,31 +4,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
+* 本模块的所有功能均为前序实现，需要按照顺序输入和遍历
+*/
+
+/* 状态定义区 */
 enum{
    OK=1,
    ERROR=-1,
    NO=0
-};
+}; 
 
-typedef struct{
+/* 数据定义区 */
+typedef struct binary_tree_data_t {
    int data;
 
 }binary_tree_data_t;
 
-typedef struct  {
-    binary_tree_data_t *data;
-    
-    struct binary_tree_node *left;
-    struct binary_tree_node *right;
 
-}binary_tree_node;
+/* 二叉树定于区 */
+typedef struct binary_tree{
 
-typedef struct  {
-    binary_tree_node* root;
-    int size;
+    binary_tree_data_t *data; 
+    struct binary_tree* left;
+    struct binary_tree* right;
+
 }binary_tree;
 
+/* 函数定义区 */
+binary_tree_data_t* binary_tree_data_input(void);
+void binary_tree_visit(binary_tree* tree,int level);
+/* 以上函数需要补充代码 */
+
 binary_tree* binary_tree_create(void);
-int binary_tree_insert (binary_tree* tree, binary_tree_data_t* data);
+int  binary_tree_traverse(binary_tree* tree,int level);
+
 
 #endif
